ec3930ce04e7ec2d7fa1a0cefc49eb99aa2daf93
paulohenriquefreitas <paulohfreitas_ti@hotmail.com>
2017-06-17 18:06:50 -0300

    Save method ~37

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -7,5 +7,9 @@ module.exports = function() {
 	this.getNoticia = function(connection, callback){
 		connection.query("select * from noticias where id_noticias = 2", callback);
 	};
+
+	this.salvarNoticia = function(noticia,connection, callback){
+		connection.query("insert into noticias set ?", noticia, callback);
+	};
 	return this
 }
--- config/server.js
+++ config/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 var app = express();
 var ejs =  require('ejs');
 
 app.set('view engine','ejs');
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 consign()
 	.include('app/routes')
